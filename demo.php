56d02756aafd017297faccd3e68285d5d440c167
krisisy <khristine.isidro@ciit.edu.ph>
2025-05-20 18:56:08 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,45 +1,45 @@
 <?php
 // Odd or Even
 
-// $num = readline("Enter a number: ");
-// $result = "Even";
+$num = readline("Enter a number: ");
+$result = "Even";
 
-// if ($num % 2 != 0){
-//      $result = "Odd";
-// }
+if ($num % 2 != 0){
+   $result = "Odd";
+ }
 
-// echo "The number is: {$result}";
+echo "The number is: {$result}";
 
 // ===========================================
 // Prime Numbers
 
-// function isPrime($num){
-//     $num = readline("Enter a number: ");
-//     if (!is_numeric($num)){
-//         return "Invalid Input";
-//         exit;
-//     }
+function isPrime($num){
+    $num = readline("Enter a number: ");
+    if (!is_numeric($num)){
+        return "Invalid Input";
+        exit;
+    }
 
-//     $num = (int)$num;
-//     for($ctr=0; $ctr <= $num; $ctr++){
-//         if ($ctr < 2){
-//             return "{$num} is not a prime number\n";
-//         }
-//         if ($ctr == 2){
-//             return "{$num} is a prime number\n";
-//         }
-//         if ($ctr % 2 == 0) {
-//             return"{$num} is not a prime number\n";
-//         }
-//         $sqrtNum = sqrt($num);
-//         for($i = 3; $i <= $sqrtNum; $i +=2 ){
-//             if ($num % $i == 0){
-//                 return "{$num} is not a prime number\n";
-//             }
-//         }
-//         return "{$num} is a prime number\n";
-//     }
-// }
+    $num = (int)$num;
+    for($ctr=0; $ctr <= $num; $ctr++){
+        if ($ctr < 2){
+            return "{$num} is not a prime number\n";
+        }
+        if ($ctr == 2){
+            return "{$num} is a prime number\n";
+        }
+        if ($ctr % 2 == 0) {
+            return"{$num} is not a prime number\n";
+        }
+        $sqrtNum = sqrt($num);
+        for($i = 3; $i <= $sqrtNum; $i +=2 ){
+            if ($num % $i == 0){
+                return "{$num} is not a prime number\n";
+            }
+        }
+        return "{$num} is a prime number\n";
+    }
+}
 
 require __DIR__ . '/vendor/autoload.php';
 use Cocur\Slugify\Slugify;
